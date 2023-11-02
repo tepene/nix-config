7b5b68e0b792808b92d31e48d1c1277aea863f5e
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-02 18:09:44 +0100

    fix: not supported

--- hosts/nixos-dev/hardware-configuration.nix
+++ hosts/nixos-dev/hardware-configuration.nix
@@ -13,11 +13,6 @@
   swapDevices =
     [ { device = "/dev/disk/by-uuid/0ff6c1d1-831b-4306-80be-8c849328a272"; }
     ];
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
 
   # Set your system kind (needed for flakes)
   nixpkgs.hostPlatform = "x86_64-linux";
