247f6edc7003a27172f952655b146074c820d3d1
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-05 22:34:42 +0100

    fix: not working

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -7,11 +7,4 @@
 
   home-manager.extraSpecialArgs = { inherit inputs outputs; };
 
-  nixpkgs = {
-    overlays = builtins.attrValues outputs.overlays;
-    config = {
-      allowUnfree = true;
-    };
-  };
-
 }
