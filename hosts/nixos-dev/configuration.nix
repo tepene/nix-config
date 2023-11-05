66591564269c3d1efe46e9b7dc3c216159aed7d2
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-05 22:25:51 +0100

    feat: setup common configuration block

--- hosts/nixos-dev/configuration.nix
+++ hosts/nixos-dev/configuration.nix
@@ -13,11 +13,13 @@
     # inputs.hardware.nixosModules.common-cpu-amd
     # inputs.hardware.nixosModules.common-ssd
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+
+    # Common configurations applied to all hosts
+    ../common/global
+
+    # Optional configurations
   ];
 
   nixpkgs = {
@@ -37,23 +39,6 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-    };
-  };
-
-  nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
     };
   };
 
