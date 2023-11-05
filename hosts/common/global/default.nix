d78000aba8ab84b9bd1cf9b7a6ae5ebadfc9ec44
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-05 22:31:28 +0100

    fix: missing default file

---
+++ hosts/common/global/default.nix
@@ -0,0 +1,17 @@
+# This file (and the global directory) holds config that i use on all hosts
+{ inputs, outputs, ... }: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ./nix.nix
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+  home-manager.extraSpecialArgs = { inherit inputs outputs; };
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+}
