30d531fec31c163ddc515253f1f82a0dd9817382
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-06 21:10:52 +0100

    feat: make nix settings available for all hosts

--- hosts/nixos-dev/configuration.nix
+++ hosts/nixos-dev/configuration.nix
@@ -10,6 +10,8 @@
 }: {
   # You can import other NixOS modules here
   imports = [
+    # common configurations
+    ./common/nix.nix
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
 
@@ -46,23 +48,6 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-    };
-  };
-
-  nix = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
     };
   };
 
