92f1ab26da9532621a357dc7b961fb8110947d4e
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-13 11:12:58 +0100

    fix: WIP - next try

--- setup.sh
+++ setup.sh
@@ -112,7 +112,7 @@ nixos-generate-config --root /mnt
 # Fixing generated config
 echo -e "${YELLOW}Add correct file system option on hardware-configuration.nix${ENDCOLOR}"
 sed -i '/^ *options = \[/ s/];/"compress=zstd" "noatime" ];/ ' /mnt/etc/nixos/hardware-configuration.nix
-sed -i '/fileSystems."\/var\/log" =/,/};/ s/options = \[/&\n      neededForBoot = true;/' /mnt/etc/nixos/hardware-configuration.nix
+sed -i '/options = \[ "subvol=log" "compress=zstd" "noatime" \];/ s/];/&\n      neededForBoot = true;/' /mnt/etc/nixos/hardware-configuration.nix
 echo ""
 
 # Finish
