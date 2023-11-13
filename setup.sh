dba8981e8f21232298e54758d9540c715804a075
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-13 11:28:52 +0100

    fix: load lvm volume at start

--- setup.sh
+++ setup.sh
@@ -111,8 +111,11 @@ nixos-generate-config --root /mnt
 
 # Fixing generated config
 echo -e "${YELLOW}Add correct file system option on hardware-configuration.nix${ENDCOLOR}"
-sed -i '/^ *options = \[/ s/];/"compress=zstd" "noatime" ];/ ' /mnt/etc/nixos/hardware-configuration.nix
-sed -i '/options = \[ "subvol=log" "compress=zstd" "noatime" \];/ s/];/&\n      neededForBoot = true;/' /mnt/etc/nixos/hardware-configuration.nix
+NIXOS_LVM_UUID=$(blkid | grep /dev/mapper/lvm-nixos | awk -F' ' '{print $2}' | cut -d '=' -f 2 -s)
+NIXOS_HW_CONFIG="/mnt/etc/nixos/hardware-configuration.nix"
+sed -i '/^ *options = \[/ s/];/"compress=zstd" "noatime" ];/ ' ${NIXOS_HW_CONFIG}
+sed -i '/options = \[ "subvol=log" "compress=zstd" "noatime" \];/ s/];/&\n      neededForBoot = true;/' ${NIXOS_HW_CONFIG}
+sed -i "/^boot.initrd/ { :a; N; \$!ba; s/.*\n/boot.initrd.luks.devices.\"nixos\".device = \"\/dev\/disk\/by-uuid\/$NIXOS_LVM_UUID\";\n&/}" yourfile
 echo ""
 
 # Finish
