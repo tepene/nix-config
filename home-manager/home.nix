bfd5d4f1a4a5d94acd36a8a8930e53c03ef882cb
Stephan Lüscher <stephan.luescher@mykolab.com>
2023-11-15 21:14:08 +0100

    style

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,7 +7,9 @@
   config,
   pkgs,
   ...
-}: {
+}:
+
+{
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
